4fff62fe14dddb38983537a4f923e72fda0c61ea
Dinesh S <dineshindevelope@gmail.com>
2024-11-06 00:30:36 +0530

    fix:cors issue

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import express from "express"
 import userRequestRoute from "./routes/user.request.js"
 import connectDB from "./lib/db.js"
+import cors from "cors";
 const app=express()
 const PORT = 5001
 
+app.use(cors());
 //Data understanding middleware
 app.use(express.json())
 app.use(express.urlencoded({ extented: true }))
